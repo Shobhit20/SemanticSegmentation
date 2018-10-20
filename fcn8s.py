ae28d4f8f0775c0d9c7e2b5242918f357dfbb58b
Shobhit20 <shobhit20897@gmail.com>
2018-10-20 19:18:24 +0530

    FCN 8s + vgg architecture

--- fcn8s.py
+++ fcn8s.py
@@ -17,11 +17,34 @@ WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/downlo
 WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 
 
+def skip_connections(skip_conn1, skip_conn2, skip_conn3, nClasses):
+    n = 4096
+    IMAGE_ORDERING = "channels_last"
+    decoder = Conv2D(n, (7, 7), activation='relu', padding='same', name="conv6")(skip_conn1)
+    conv7 = (Conv2D(n, (1, 1), activation='relu', padding='same', name="conv7", data_format=IMAGE_ORDERING))(decoder)
+
+    # Deconvolution Layer
+    conv7_4 = Conv2DTranspose(nClasses, kernel_size=(4, 4), strides=(4, 4), use_bias=False,
+                              data_format=IMAGE_ORDERING)(conv7)
+
+    pool411 = Conv2D(nClasses, (1, 1), activation='relu', padding='same', name="pool4_11",
+                     data_format=IMAGE_ORDERING)(skip_conn2)
+    pool411_2 = Conv2DTranspose(nClasses, kernel_size=(2, 2), strides=(2, 2), use_bias=False,
+                                data_format=IMAGE_ORDERING)(pool411)
+
+    pool311 = Conv2D(nClasses, (1, 1), activation='relu', padding='same', name="pool3_11",
+                     data_format=IMAGE_ORDERING)(skip_conn3)
+
+    decoder = Add(name="add")([pool411_2, pool311, conv7_4])
+    decoder = Conv2DTranspose(nClasses, kernel_size=(8, 8), strides=(8, 8), use_bias=False, data_format=IMAGE_ORDERING)(decoder)
+    decoder = (Activation('softmax'))(decoder)
+    return decoder
+
+
 def VGG16_backbone(nClasses=20):
 
     img_input = Input(shape=(224, 224, 3))
-    img_input1 = Input(shape=(224, 224, 3))
-    IMAGE_ORDERING = "channels_last"
+
 
     # Block 1
     x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(img_input)
@@ -38,7 +61,7 @@ def VGG16_backbone(nClasses=20):
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv2')(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same', name='block3_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
-    skip_conn_pool1 = x
+    skip_conn_pool3 = x
 
     # Block 4
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block4_conv1')(x)
@@ -52,7 +75,7 @@ def VGG16_backbone(nClasses=20):
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv2')(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same', name='block5_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
-    skip_conn_pool3 = x
+    skip_conn_pool1 = x
 
     model3 = Model(img_input, x, name='vgg16')
 
@@ -62,29 +85,10 @@ def VGG16_backbone(nClasses=20):
 
     model3.load_weights(weights_path)
 
-    n = 4096
-    o = (Conv2D(n, (7, 7), activation='relu', padding='same', name="conv6"))(skip_conn_pool3)
-    conv7 = (Conv2D(n, (1, 1), activation='relu', padding='same', name="conv7", data_format=IMAGE_ORDERING))(o)
+    decoder_architecture = skip_connections(skip_conn_pool1, skip_conn_pool2, skip_conn_pool3, nClasses)
 
-    ## 4 times upsamping for pool4 layer
-    conv7_4 = Conv2DTranspose(nClasses, kernel_size=(4, 4), strides=(4, 4), use_bias=False,
-                              data_format=IMAGE_ORDERING)(conv7)
-    ## (None, 224, 224, 10)
-    ## 2 times upsampling for pool411
-    pool411 = Conv2D(nClasses, (1, 1), activation='relu', padding='same', name="pool4_11",
-                     data_format=IMAGE_ORDERING)(skip_conn_pool2)
-    pool411_2 = Conv2DTranspose(nClasses, kernel_size=(2, 2), strides=(2, 2), use_bias=False,
-                                data_format=IMAGE_ORDERING)(pool411)
+    model = Model(img_input, decoder_architecture)
 
-    pool311 = Conv2D(nClasses, (1, 1), activation='relu', padding='same', name="pool3_11",
-                     data_format=IMAGE_ORDERING)(skip_conn_pool1)
-
-    o = Add(name="add")([pool411_2, pool311, conv7_4])
-    o = Conv2DTranspose(nClasses, kernel_size=(8, 8), strides=(8, 8), use_bias=False, data_format=IMAGE_ORDERING)(o)
-    o = (Activation('softmax'))(o)
-
-    model2 = Model(img_input1, o)
-
-    return model2
+    return model
 
 
